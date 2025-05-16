14105f2fdfd0bb768d49573f5564dc09a7791de5
Howard Fan <howard.j.fan@gmail.com>
2025-05-16 17:20:50 -0400

    updated kBET
    Added optional kBET parameters to run_kBET call, added roxygen comments to plot_kBET, added parameter variable types in comments for run_kBET, ran roxygen2::roxygenise()

--- R/kBET.R
+++ R/kBET.R
@@ -3,37 +3,65 @@
 #' evaluate whether the data has detectable batch effect.
 #'
 #' @param se SummarizedExperiment object
-#' @param batch The variable that represents batch
-#' @param k0 number of nearest neighbors to test on (neighborhood size)
+#' @param batch character string of column name that represents batch
+#' @param k0 integer representing number of nearest neighbors to test on 
+#' (neighborhood size)
 #' @param knn n x k matrix of nearest neighbors for each cell (optional)
-#' @param testSize number of data points to test
-#' @param do.pca if TRUE, perform a pca prior to knn search
+#' @param testSize integer representing number of data points to test
+#' @param do.pca Boolean, if TRUE, perform a pca prior to knn search
 #' (defaults to TRUE)
-#' @param dim.pca if do.pca=TRUE, choose the number of dimensions to consider
-#' (defaults to 50)
-#' @param heuristic heuristic compute an optimal neighborhood size k
+#' @param dim.pca Boolean, if do.pca=TRUE, choose the number of dimensions to 
+#' consider (defaults to 50)
+#' @param heuristic Boolean, if true, compute an optimal neighborhood size k
 #' (defaults to TRUE)
-#' @param n_repeat to create a statistics on batch estimates, evaluate 
-#' 'n_repeat' subsets
-#' @param alpha significance level
-#' @param addTest perform an LRT-approximation to the multinomial test AND a 
-#' multinomial exact test (if appropriate)
-#' @param verbose if TRUE, display stages of current computation
+#' @param n_repeat numeric representing 'n_repeat' subsets to evaluate in order 
+#' to create a statistics on batch estimates
+#' @param alpha numeric for significance level
+#' @param addTest Boolean, if TRUE, perform an LRT-approximation to the 
+#' multinomial test AND a multinomial exact test (if appropriate)
+#' @param verbose Boolean, if TRUE, display stages of current computation
 #' (defaults to FALSE)
-#' @param adapt In some cases, a number of cells do not contribute
-#' to any neighbourhood and this may cause an imbalance in observed and 
-#' expected batch label frequencies.
+#' @param adapt Boolean, if TRUE, frequencies will be adapted (defaults to TRUE)
 #' 
+#' @returns list object from kBET() function
+#'    \enumerate{
+#'    \item \code{summary} - a rejection rate for the data,
+#'         an expected rejection rate for random
+#'         labeling and the significance for the observed result
+#'    \item \code{results} - detailed list for each tested cells;
+#'         p-values for expected and observed label distribution
+#'    \item \code{average.pval} - significance level over the averaged
+#'         batch label distribution in all neighbourhoods
+#'    \item \code{stats} - extended test summary for every sample
+#'    \item \code{params} - list of input parameters and adapted parameters,
+#'    respectively
+#'    \item \code{outsider} - only shown if \code{adapt=TRUE}. List of samples
+#'         without mutual nearest neighbour: \itemize{
+#'     \item \code{index} - index of each outsider sample)
+#'     \item \code{categories} - tabularised labels of outsiders
+#'     \item \code{p.val} - Significance level of outsider batch label distribution
+#'         vs expected frequencies.
+#'     If the significance level is lower than \code{alpha},
+#'     expected frequencies will be adapted}
+#'    }
 #' @import SummarizedExperiment
 #' @import kBET
 #' @import ggplot2
 #' 
+#' @examples
+#' library(scran)
+#' se <- mockSCE()
+#' kBET_result <- BatchQC::run_kBET(se = se,
+#'                                  assay_to_normalize = "counts",
+#'                                  batch = "Treatment")
+#'                                  
+#' BatchQC::plot_kBET(kBET_result)
+#'
 #' @export
-
-run_kBET <- function(se, assay_to_normalize, batch, k0 = NULL, knn = NULL, testSize = NULL,
-                     do.pca = TRUE, dim.pca = 50, heuristic = TRUE,
-                     n_repeat = 100, alpha = 0.05, addTest = FALSE,
-                     verbose = FALSE, adapt = TRUE){
+run_kBET <- function(se, assay_to_normalize, batch, k0 = NULL, knn = NULL, 
+                     testSize = NULL, do.pca = TRUE, dim.pca = 50,
+                     heuristic = TRUE, n_repeat = 100, alpha = 0.05,
+                     addTest = FALSE, verbose = FALSE, adapt = TRUE){
   
   # run kBET
   batch.estimate <- kBET::kBET(df = as.matrix(assays(se)[[assay_to_normalize]]),
@@ -47,6 +75,16 @@ run_kBET <- function(se, assay_to_normalize, batch, k0 = NULL, knn = NULL, testS
   
   return(batch.estimate)
 }
+
+
+#' kBET Rejection Plotter
+#' This function generates a boxplot of observed and expected rejection rates 
+#' for the provided kBET output list object
+#'
+#' @param kBET_res list object output from kBET function
+#'
+#' @returns ggplot object containing kBET rejection boxplot
+#' @export
 plot_kBET <- function(kBET_res){
   # create ggplot object for plotting kBET's rejection rate
   plot.data <- data.frame(class=rep(c('observed', 'expected'), 
--- inst/shiny/server/server_09_kbet.R
+++ inst/shiny/server/server_09_kbet.R
@@ -9,7 +9,11 @@ observeEvent(input$run_kBET, {
     results <- run_kBET(se = reactivevalue$se,
                         assay_to_normalize = input$kbet_assay,
                         batch = input$kbet_batch,
-                        k0 = input$knet_k0)
+                        k0 = input$kbet_k0,
+                        do.pca = (input$kbet_doPCA == 'Yes'),
+                        dim.pca = input$kbet_dimPCA,
+                        heuristic = (input$kbet_heuristic == 'Yes'),
+                        alpha = input$kbet_alpha)
     setProgress(.8, 'Displaying figure...')
     output$kBET_plot <- renderPlot({
       plot_kBET(results)})
