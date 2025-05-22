ade049a9ee343865c5fffe61a6784e6956a57898
Howard Fan <howard.j.fan@gmail.com>
2025-05-21 22:09:46 -0400

    Reformatted with styler

--- R/kBET.R
+++ R/kBET.R
@@ -1,28 +1,28 @@
 #' kBET rejection rate
-#' This function runs the k-nearest neighbor batch effect test (kBET) to 
+#' This function runs the k-nearest neighbor batch effect test (kBET) to
 #' evaluate whether the data has detectable batch effect.
 #'
 #' @param se SummarizedExperiment object
 #' @param batch character string of column name that represents batch
-#' @param k0 integer representing number of nearest neighbors to test on 
+#' @param k0 integer representing number of nearest neighbors to test on
 #' (neighborhood size)
 #' @param knn n x k matrix of nearest neighbors for each cell (optional)
 #' @param testSize integer representing number of data points to test
 #' @param do.pca Boolean, if TRUE, perform a pca prior to knn search
 #' (defaults to TRUE)
-#' @param dim.pca Boolean, if do.pca=TRUE, choose the number of dimensions to 
+#' @param dim.pca Boolean, if do.pca=TRUE, choose the number of dimensions to
 #' consider (defaults to 50)
 #' @param heuristic Boolean, if true, compute an optimal neighborhood size k
 #' (defaults to TRUE)
-#' @param n_repeat numeric representing 'n_repeat' subsets to evaluate in order 
+#' @param n_repeat numeric representing 'n_repeat' subsets to evaluate in order
 #' to create a statistics on batch estimates
 #' @param alpha numeric for significance level
-#' @param addTest Boolean, if TRUE, perform an LRT-approximation to the 
+#' @param addTest Boolean, if TRUE, perform an LRT-approximation to the
 #' multinomial test AND a multinomial exact test (if appropriate)
 #' @param verbose Boolean, if TRUE, display stages of current computation
 #' (defaults to FALSE)
 #' @param adapt Boolean, if TRUE, frequencies will be adapted (defaults to TRUE)
-#' 
+#'
 #' @returns list object from kBET() function
 #'    \enumerate{
 #'    \item \code{summary} - a rejection rate for the data,
@@ -47,54 +47,63 @@
 #' @import SummarizedExperiment
 #' @import kBET
 #' @import ggplot2
-#' 
+#'
 #' @examples
 #' library(scran)
 #' se <- mockSCE()
-#' kBET_result <- BatchQC::run_kBET(se = se,
-#'                                  assay_to_normalize = "counts",
-#'                                  batch = "Treatment")
-#'                                  
+#' kBET_result <- BatchQC::run_kBET(
+#'   se = se,
+#'   assay_to_normalize = "counts",
+#'   batch = "Treatment"
+#' )
+#'
 #' BatchQC::plot_kBET(kBET_result)
 #'
 #' @export
-run_kBET <- function(se, assay_to_normalize, batch, k0 = NULL, knn = NULL, 
+run_kBET <- function(se, assay_to_normalize, batch, k0 = NULL, knn = NULL,
                      testSize = NULL, do.pca = TRUE, dim.pca = 50,
                      heuristic = TRUE, n_repeat = 100, alpha = 0.05,
-                     addTest = FALSE, verbose = FALSE, adapt = TRUE){
-  
+                     addTest = FALSE, verbose = FALSE, adapt = TRUE) {
   # run kBET
-  batch.estimate <- kBET::kBET(df = as.matrix(assays(se)[[assay_to_normalize]]),
-                               batch = data.frame(colData(se))[, batch],
-                               k0 = k0, knn = knn,
-                               testSize = testSize, do.pca = do.pca,
-                               dim.pca = dim.pca, heuristic = heuristic,
-                               n_repeat = n_repeat, alpha = alpha,
-                               addTest = addTest, verbose = verbose,
-                               plot = FALSE, adapt = adapt)
-  
+  batch.estimate <- kBET::kBET(
+    df = as.matrix(assays(se)[[assay_to_normalize]]),
+    batch = data.frame(colData(se))[, batch],
+    k0 = k0, knn = knn,
+    testSize = testSize, do.pca = do.pca,
+    dim.pca = dim.pca, heuristic = heuristic,
+    n_repeat = n_repeat, alpha = alpha,
+    addTest = addTest, verbose = verbose,
+    plot = FALSE, adapt = adapt
+  )
+
   return(batch.estimate)
 }
 
 
 #' kBET Rejection Plotter
-#' This function generates a boxplot of observed and expected rejection rates 
+#' This function generates a boxplot of observed and expected rejection rates
 #' for the provided kBET output list object
 #'
 #' @param kBET_res list object output from kBET function
 #'
 #' @returns ggplot object containing kBET rejection boxplot
 #' @export
-plot_kBET <- function(kBET_res){
+plot_kBET <- function(kBET_res) {
   # create ggplot object for plotting kBET's rejection rate
-  plot.data <- data.frame(class=rep(c('observed', 'expected'), 
-                                    each=length(kBET_res$stats$kBET.observed)), 
-                          data =  c(kBET_res$stats$kBET.observed,
-                                    kBET_res$stats$kBET.expected))
-  g <- ggplot(plot.data, aes(class, data)) + geom_boxplot() + 
-    labs(x='Test', y='Rejection rate',title='kBET test results') +
-    theme_bw() +  
-    scale_y_continuous(limits=c(0,1))
-  
+  plot.data <- data.frame(
+    class = rep(c("observed", "expected"),
+      each = length(kBET_res$stats$kBET.observed)
+    ),
+    data = c(
+      kBET_res$stats$kBET.observed,
+      kBET_res$stats$kBET.expected
+    )
+  )
+  g <- ggplot(plot.data, aes(class, data)) +
+    geom_boxplot() +
+    labs(x = "Test", y = "Rejection rate", title = "kBET test results") +
+    theme_bw() +
+    scale_y_continuous(limits = c(0, 1))
+
   return(g)
 }
--- inst/shiny/server/server_09_kbet.R
+++ inst/shiny/server/server_09_kbet.R
@@ -3,20 +3,23 @@
 ## run kBET
 observeEvent(input$run_kBET, {
   req(reactivevalue$se, input$kbet_assay, input$kbet_batch)
-  msg <- sprintf('Generating plot for: %s...', input$kbet_assay)
+  msg <- sprintf("Generating plot for: %s...", input$kbet_assay)
   withProgress(message = msg, {
-    setProgress(.5, 'Running  kBET...')
-    results <- run_kBET(se = reactivevalue$se,
-                        assay_to_normalize = input$kbet_assay,
-                        batch = input$kbet_batch,
-                        k0 = input$kbet_k0,
-                        do.pca = (input$kbet_doPCA == 'Yes'),
-                        dim.pca = input$kbet_dimPCA,
-                        heuristic = (input$kbet_heuristic == 'Yes'),
-                        alpha = input$kbet_alpha)
-    setProgress(.8, 'Displaying figure...')
+    setProgress(.5, "Running  kBET...")
+    results <- run_kBET(
+      se = reactivevalue$se,
+      assay_to_normalize = input$kbet_assay,
+      batch = input$kbet_batch,
+      k0 = input$kbet_k0,
+      do.pca = (input$kbet_doPCA == "Yes"),
+      dim.pca = input$kbet_dimPCA,
+      heuristic = (input$kbet_heuristic == "Yes"),
+      alpha = input$kbet_alpha
+    )
+    setProgress(.8, "Displaying figure...")
     output$kBET_plot <- renderPlot({
-      plot_kBET(results)})
-    setProgress(1, 'Complete.')
+      plot_kBET(results)
+    })
+    setProgress(1, "Complete.")
   })
 })
